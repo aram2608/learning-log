4e99907bbfe1074295853f8638ee650070c68d92
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-06-02 22:38:27 -0400

    the directory structure for making this is so wacky wtf

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path # needed to map URLs to views
+
+from . import views
+
+app_name = 'learning_logs' # helps Django distinguish this app from the others in the project
+urlpatterns = [
+    # Home Page
+    path('', views.index, name='index'), # empty string for now to match the base URL
+]
