6957d2eb1f78b47eb1c14b9de35948c46bd90653
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-06-04 21:58:13 -0400

    stylized site

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -10,7 +12,7 @@ def index(request):
 @login_required
 def topics(request):
     """The topics page for the Learning Logs."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics} # a context is a dictionary in which keys are names well use in the template to access data we want, and the values are data we want to send
     return render(request, 'learning_logs/topics.html', context)
 
@@ -18,6 +20,9 @@ def topics(request):
 def topic(request, topic_id): # this parameter gets captured by /<int:topic_id>/
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -32,7 +37,9 @@ def new_topic(request):
         # Post data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid(): # checks to see if submission is valid
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics') # redirects user to topics page after posting
         
     # Display a blank or invalid form
@@ -41,8 +48,10 @@ def new_topic(request):
 
 @login_required
 def new_entry(request, topic_id):
-    """Add a new entry for a particular topic"""
+    """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # No data submitted, create a blank form
@@ -64,6 +73,8 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
